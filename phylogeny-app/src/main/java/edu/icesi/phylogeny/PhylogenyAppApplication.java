29eff910ca7c976ddb1fbde044fac51c7fa64bd7
Simon <157841549+SimonGarcia01@users.noreply.github.com>
2026-04-08 18:28:09 -0500

    Added the basic initialized springboot project.

---
+++ phylogeny-app/src/main/java/edu/icesi/phylogeny/PhylogenyAppApplication.java
@@ -0,0 +1,13 @@
+package edu.icesi.phylogeny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhylogenyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhylogenyAppApplication.class, args);
+    }
+
+}
